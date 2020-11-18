7f83c52a95c190d1f65c9aa4d0388eb1da9164c0
Stiven Pulgarin <s.pulgarin@utp.edu.co>
2020-11-17 22:23:54 -0500

    Personalización login v2.0

--- db/migrate/20201110225239_devise_create_users.rb
+++ db/migrate/20201110225239_devise_create_users.rb
@@ -32,7 +32,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      # Lockable
+      t.string :name
       t.timestamps null: false
     end
 
