c51cbf2fed37cc4903b6a8cc589d3f9d9a8bcd51
Janier Chara Cardona <j.chara@utp.edu.co>
2020-11-23 02:20:26 -0500

    CRUD Productores [add] se creo el parcial para los formularios new y edit de Prodcutores [add] se creo el view para la lista de prodcutores creados [update] se modifico el layout de inicio [update] se modifico el css de  de prodcutores

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,3 +1,7 @@
 class Producer < ApplicationRecord
     has_many :nurseries
+    validates :document_type, presence: true
+    validates :document_number, presence: true, uniqueness: true, format: {with: /\d/ }
+    validates :name, presence: true, format: {with: /\D/ }
+    validates :lastname, presence: true, format: {with: /\D/ }
 end
