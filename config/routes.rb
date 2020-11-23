18958933ea88c764c41e2696337c4e8c34ee61be
Janier Chara Cardona <j.chara@utp.edu.co>
2020-11-23 02:20:26 -0500

    [update] se modifico el layout de inicio [add] se creo el parcial para los formularios new y edit de Prodcutore [add] se agrego CRUD Prodcutore [fix] validaacion en la creacion de nuevos productores

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
 
+  
   devise_for :users
-  get 'welcome/index'
+  #get 'producers/producers'
+  #get 'welcome/index'
+  
+  resources :producers
+=begin
+  get "/producers" index
+  post "/producers" create
+  delete "/producers/:id" destroy
+  get "/producers/:id" show
+  get "/producers/new" new 
+  get "/producers/:id/edit" edit
+  path "/producers/:id" update
+  put "/producers/:id" update
+=end
 
-  root :to => 'welcome#index'
+  root :to => 'welcome#index'  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
