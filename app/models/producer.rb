f08a67912e0c19f3abafbc8fb147f5bf712e14ab
Daniela Jaramillo Perez <danielaj22p@gmail.com>
2020-11-23 23:14:41 -0500

    cambio de diseño y validaciones

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,3 +1,7 @@
 class Producer < ApplicationRecord
     has_many :nurseries
+    validates :document_type, presence: true
+    validates :document_number, presence: true, uniqueness: true
+    validates :name, presence: true
+    validates :lastname, presence: true
 end
